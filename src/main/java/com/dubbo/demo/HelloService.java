2d949c8dd84656b88bf02f794ab84371746d215f
root <admin@example.com>
2016-11-25 20:16:07 +0800

    1.show example fot map and object echo

--- src/main/java/com/dubbo/demo/HelloService.java
+++ src/main/java/com/dubbo/demo/HelloService.java
@@ -1,6 +1,10 @@
 package com.dubbo.demo;
 
 
+import java.util.Map;
+
 public interface HelloService {
     String hello(String name);
+    Map<String,String> mapEcho();
+    TransitionNode objectEcho();
 }
--- src/main/java/com/dubbo/demo/impl/HelloServiceImpl.java
+++ src/main/java/com/dubbo/demo/impl/HelloServiceImpl.java
@@ -1,6 +1,10 @@
 package com.dubbo.demo.impl;
 
 import com.dubbo.demo.HelloService;
+import com.dubbo.demo.TransitionNode;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class HelloServiceImpl implements HelloService {
@@ -8,5 +12,18 @@ public class HelloServiceImpl implements HelloService {
     public String hello(String name) {
         System.out.println("getContent " + name);
         return "Hello, " + name + "!";
+    }
+
+    @Override
+    public Map<String, String> mapEcho() {
+        Map<String,String> ret= new HashMap<String,String>();
+        ret.put("key1","Hello");
+        ret.put("key2","World");
+        return ret;
+    }
+
+    @Override
+    public TransitionNode objectEcho() {
+        return new TransitionNode();
     }
 }
