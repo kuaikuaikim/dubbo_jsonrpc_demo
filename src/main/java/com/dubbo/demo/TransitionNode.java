703a59ad09813ba2b749e51003acfd6894076f2c
root <admin@example.com>
2017-02-21 11:00:32 +0800

    1.add TransationNode

---
+++ src/main/java/com/dubbo/demo/TransitionNode.java
@@ -0,0 +1,13 @@
+package com.dubbo.demo;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by jake on 16-11-25.
+ */
+public class TransitionNode implements Serializable{
+    public String key1 = "hello world";
+    public String key2 = "2016";
+}
